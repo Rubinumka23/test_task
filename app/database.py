f0affceaba0fb2a15ef6a5841aa928b48c87da86
Rubinumka23 <74733966+Rubinumka23@users.noreply.github.com>
2022-08-19 02:02:24 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("HOST_DB")}:{os.getenv("PORT")}/{os.getenv("POSTGRES_DB")}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("HOST_DB")}:5432/{os.getenv("POSTGRES_DB")}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
